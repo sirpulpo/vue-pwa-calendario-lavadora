97842e1d2b2a41a5e444889f3c642a1e1a55a101
sirpulpo <taz.vd@hotmail.com>
2026-09-22 22:52:33 -0600

    Fix en el calendario

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,25 +4,26 @@
       <v-toolbar flat color="transparent" density="comfortable">
         <v-btn icon="mdi-chevron-left" variant="text" aria-label="Mes anterior" @click="prevMonth" />
 
-        <v-toolbar-title class="text-capitalize text-center flex-grow-0 mx-2">
+        <v-spacer />
+
+        <v-toolbar-title class="text-capitalize text-center flex-grow-0 mx-2 text-h6">
           {{ monthLabel }}
         </v-toolbar-title>
 
-        <v-btn icon="mdi-chevron-right" variant="text" aria-label="Mes siguiente" @click="nextMonth" />
-
         <v-spacer />
 
-        <v-btn variant="tonal" color="primary" prepend-icon="mdi-calendar-today" @click="goToday">
+        <v-btn icon="mdi-chevron-right" variant="text" aria-label="Mes siguiente" @click="nextMonth" />
+
+        <v-btn variant="tonal" color="primary" prepend-icon="mdi-calendar-today" class="ml-2" @click="goToday">
           Hoy
         </v-btn>
       </v-toolbar>
 
       <v-calendar
         v-model="focus"
-        view-mode="month"
+        type="month"
         :weekdays="[1, 2, 3, 4, 5, 6, 0]"
         :events="events"
-        hide-header
         class="mt-2"
       />
     </v-sheet>
@@ -35,23 +36,23 @@ import { useDate } from 'vuetify'
 
 const dateAdapter = useDate()
 
-const focus = ref([new Date()])
+const focus = ref(new Date())
 const events = ref([])
 
 const monthLabel = computed(() => {
-  const label = dateAdapter.format(focus.value[0], 'monthAndYear')
+  const label = dateAdapter.format(focus.value, 'monthAndYear')
   return label.charAt(0).toUpperCase() + label.slice(1)
 })
 
 function prevMonth() {
-  focus.value = [dateAdapter.addMonths(focus.value[0], -1)]
+  focus.value = dateAdapter.addMonths(focus.value, -1)
 }
 
 function nextMonth() {
-  focus.value = [dateAdapter.addMonths(focus.value[0], 1)]
+  focus.value = dateAdapter.addMonths(focus.value, 1)
 }
 
 function goToday() {
-  focus.value = [new Date()]
+  focus.value = new Date()
 }
 </script>
