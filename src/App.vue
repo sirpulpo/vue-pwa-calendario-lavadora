216e551879aae4679b071a866ebade453a083eb6
sirpulpo <taz.vd@hotmail.com>
2026-09-23 00:52:36 -0600

    Se elmina la pagina de about

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,25 @@
 import { ref } from 'vue'
 import { useAppStore } from '@/stores/app'
 
+import IconVue from '~icons/logos/vue'
+import IconVitejs from '~icons/logos/vitejs'
+import IconVuetify from '~icons/logos/vuetifyjs'
+import IconPinia from '~icons/logos/pinia'
+import IconPwa from '~icons/logos/pwa'
+
 const store = useAppStore()
 
 const drawer = ref(false)
 const confirmarLogout = ref(false)
 const snackbar = ref(false)
+
+const iconComponents = {
+  vue: IconVue,
+  vitejs: IconVitejs,
+  vuetifyjs: IconVuetify,
+  pinia: IconPinia,
+  pwa: IconPwa,
+}
 
 function abrirConfirmarLogout() {
   confirmarLogout.value = true
@@ -25,11 +39,11 @@ function logout() {
     <v-app-bar color="primary" density="comfortable">
       <v-icon icon="mdi-calendar" class="ml-3 mr-2" style="cursor: pointer" @click="$router.push({ name: 'home' })" />
       <v-app-bar-title>{{ store.appName }}</v-app-bar-title>
-      <template v-if="store.usuario">
-        <span class="mr-2 d-none d-sm-inline">{{ store.usuario.nombre }}</span>
-        <v-btn icon="mdi-account-circle" aria-label="Usuario" @click="drawer = !drawer" />
-      </template>
-      <v-btn icon="mdi-information-outline" aria-label="Acerca de" :to="{ name: 'about' }" />
+      <v-btn v-if="store.usuario" variant="text" class="mr-1" aria-label="Usuario" @click="drawer = !drawer">
+        <span class="d-none d-sm-inline mr-2 text-none">{{ store.usuario.nombre }}</span>
+        <v-icon icon="mdi-account-circle" size="large" />
+      </v-btn>
+      <!-- <v-btn icon="mdi-information-outline" aria-label="Acerca de" :to="{ name: 'about' }" /> -->
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" location="right" temporary width="300">
@@ -53,6 +67,29 @@ function logout() {
           <v-btn block color="error" variant="tonal" prepend-icon="mdi-logout" @click="abrirConfirmarLogout">
             Cerrar sesión
           </v-btn>
+        </div>
+
+        <v-divider />
+
+        <div class="pa-3 text-center">
+          <div class="text-subtitle-2 font-weight-bold">Tecnologías</div>
+          <div class="text-caption text-medium-emphasis mb-2">{{ store.appName }} está construido con</div>
+          <div class="d-flex flex-wrap justify-center ga-2">
+            <v-card
+              v-for="tech in store.technologies"
+              :key="tech.name"
+              :href="tech.url"
+              target="_blank"
+              rel="noopener"
+              variant="tonal"
+              hover
+              width="80"
+              class="py-2 text-center"
+            >
+              <component :is="iconComponents[tech.icon]" style="font-size: 24px" />
+              <div class="text-caption mt-1">{{ tech.name }}</div>
+            </v-card>
+          </div>
         </div>
       </template>
     </v-navigation-drawer>
--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,6 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    {
-      path: '/acerca',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
   ],
 })
 
