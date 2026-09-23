ecd9daab5445cb92bb589d8e83c2540437c7c060
sirpulpo <taz.vd@hotmail.com>
2026-09-23 00:25:59 -0600

    Se agrega nombre e icono de usuario

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,10 @@ const store = useAppStore()
 <template>
   <v-app>
     <v-app-bar color="primary" density="comfortable">
+      <v-icon icon="mdi-calendar" class="ml-3 mr-2" style="cursor: pointer" @click="$router.push({ name: 'home' })" />
       <v-app-bar-title>{{ store.appName }}</v-app-bar-title>
+      <span class="mr-2">Victor Daniel Aguirre Gil</span>
+      <v-btn icon="mdi-account-circle" aria-label="Usuario" />
       <v-btn icon="mdi-information-outline" aria-label="Acerca de" :to="{ name: 'about' }" />
     </v-app-bar>
 
